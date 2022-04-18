f41b6010b6d863d1207d0e3ba3440dc554f54184
Dilan Joel Bergano Caicedo <=>
2022-04-18 09:46:34 -0500

    First Commit

---
+++ src/main/java/com/casalibertad/CasaLibertadApplication.java
@@ -0,0 +1,13 @@
+package com.casalibertad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaLibertadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaLibertadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casalibertad/CasaLibertadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casalibertad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaLibertadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
